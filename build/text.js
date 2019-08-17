e1b235a667bbccfcfe2fc481b84c4c848344e79d
Victor Aremu <victor.olorunbunmi@gmail.com>
2019-08-17 15:21:46 +0100

    dist update

--- build/text.js
+++ build/text.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const TextTemplate = {
     errorTitle: "Yikes 🤔",
-    processNotFound: (name) => `It seems like ${name} is yet to be opened. Click to launch app! 🚀`,
+    processNotFound: (name) => `It seems like ${name.split('.exe')[0]} is yet to be opened. Click to launch app! 🚀`,
     noHotApp: (key) => `No hot app as been assigned to NUM ${key}, add one at the Switch dock! 👆 `
 };
 exports.default = TextTemplate;
