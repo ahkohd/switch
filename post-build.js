7a11c09f431b71614f52aa25fea7e1c63283bb1b
Victor Aremu <victor.olorunbunmi@gmail.com>
2019-08-26 13:47:38 +0100

    automatic relase to github

--- post-build.js
+++ post-build.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const zipFolder = require('zip-folder');
+const ghRelease = require('gh-release');
 
 console.log("SWITCH SERVICE POST BUILD");
 // create bin folder if does not exists
 console.log('[info]: Checking ./bin');
-if(!fs.existsSync('./bin')) {
+if (!fs.existsSync('./bin')) {
     console.log('[info]: ./bin not found!');
     console.log('[info]: Created ./bin');
     fs.mkdirSync('./bin');
@@ -12,40 +13,86 @@ if(!fs.existsSync('./bin')) {
 
 // create bin/notifier folder if does not exists
 console.log('[info]: Checking ./bin/notifier');
-if(!fs.existsSync('./bin/notifier')) {
+if (!fs.existsSync('./bin/notifier')) {
     console.log('[info]: ./bin/notifier not found!');
     console.log('[info]: Created ./bin/notifier');
     fs.mkdirSync('./bin/notifier');
 }
 
-const list = [
-    {from: './node_modules/node-notifier/vendor/notifu/notifu.exe', to: 'bin/notifier/notifu.exe'},
-    {from: './node_modules/node-notifier/vendor/notifu/notifu64.exe', to: 'bin/notifier/notifu64.exe'},
-    {from: './node_modules/node-notifier/vendor/snoreToast/SnoreToast.exe', to: 'bin/notifier/SnoreToast.exe'},
-    {from: './node_modules/node-notifier/vendor/mac.noindex/terminal-notifier.app/Contents/MacOS/terminal-notifier', to: 'bin/notifier/terminal-notifier'},
-    {from: './node_modules/iohook/builds/node-v64-win32-x64/build/Release/iohook.node', to: 'bin/iohook.node'},
-    {from: './node_modules/iohook/builds/node-v64-win32-x64/build/Release/uiohook.dll', to: 'bin/uiohook.dll'},
-    {from: './node_modules/node-window-manager/build/Release/addon.node', to: 'bin/addon.node'}
+const list = [{
+        from: './node_modules/node-notifier/vendor/notifu/notifu.exe',
+        to: 'bin/notifier/notifu.exe'
+    },
+    {
+        from: './node_modules/node-notifier/vendor/notifu/notifu64.exe',
+        to: 'bin/notifier/notifu64.exe'
+    },
+    {
+        from: './node_modules/node-notifier/vendor/snoreToast/SnoreToast.exe',
+        to: 'bin/notifier/SnoreToast.exe'
+    },
+    {
+        from: './node_modules/node-notifier/vendor/mac.noindex/terminal-notifier.app/Contents/MacOS/terminal-notifier',
+        to: 'bin/notifier/terminal-notifier'
+    },
+    {
+        from: './node_modules/iohook/builds/node-v64-win32-x64/build/Release/iohook.node',
+        to: 'bin/iohook.node'
+    },
+    {
+        from: './node_modules/iohook/builds/node-v64-win32-x64/build/Release/uiohook.dll',
+        to: 'bin/uiohook.dll'
+    },
+    {
+        from: './node_modules/node-window-manager/build/Release/addon.node',
+        to: 'bin/addon.node'
+    }
 ];
 
 
 list.forEach(path => {
-    if(fs.existsSync(path.from))
-    {
+    if (fs.existsSync(path.from)) {
         console.log(`[info]: Copy ${path.from}`);
         fs.copyFile(path.from, path.to, (err) => {
             if (err) throw err;
             console.log(`[info]: Copied ${path.to}`);
-          });
+        });
     }
 });
 
 console.log('[info]: Packing Binaries');
+if (!process.env.GH_TOKEN) {
+    console.log('[info]: GH_TOKEN not set, Aborted release!');
+    return;
+}
+
 const getVersion = JSON.parse(fs.readFileSync('./package.json')).version;
-zipFolder('./bin', `./switch_deamon_v${getVersion}_${process.platform}.zip`, function(err) {
-    if(err) {
+zipFolder('./bin', `./switch_deamon_v${getVersion}_${process.platform}.zip`, function (err) {
+    if (err) {
         console.log('[info]: Packing failed', err);
     } else {
         console.log('[success]: BUILD SUCCEED!');
     }
+});
+
+console.log('[info]: Releasing to github');
+var options = {
+    tag_name: getVersion,
+    target_commitish: 'master',
+    name: 'v1.0.0',
+    body: '* Update\n',
+    draft: true,
+    prerelease: true,
+    repo: 'switch',
+    owner: 'ahkohd',
+    endpoint: 'https://api.github.com' // for GitHub enterprise, use http(s)://hostname/api/v3
+};
+
+options.auth = {
+    token: process.env.GH_TOKEN
+};
+
+ghRelease(options, function (err, result) {
+    if (err) throw err
+    console.log('[success]: Released to github');
 });
