f3c351ab9e036a07e627404b38a496d9012650b1
ahkohd <victor.olorunbunmi@gmail.com>
2019-10-23 22:14:57 +0100

    Finalize MacOS Implementation

--- src/switch.ts
+++ src/switch.ts
@@ -10,8 +10,7 @@ import {
     saveConfig,
     getConfig,
     checkDevMode,
-    switchLog,
-    bingSwitchToFocus
+    switchLog
 } from './utils';
 
 import { SwitchHotApp, Settings } from './interfaces';
@@ -28,7 +27,7 @@ const ioHook = require('iohook');
 let clientPID = null;
 let hotapps: SwitchHotApp[] = getHotApps();
 let config = getConfig();
-const log = switchLog.bind({isDevMode: checkDevMode()});
+const log = switchLog.bind({ isDevMode: checkDevMode() });
 let disableKeyUpListen = false;
 let disableKeyUpListenTimeout;
 
@@ -55,12 +54,11 @@ function react(event) {
         }
     } else {
         // if not hot app found make the client active..
-    
-        if(process.platform == 'darwin')
-        {
+
+        if (process.platform == 'darwin') {
             if (event.keycode >= 0 && event.keycode <= 9) {
                 interChannel.sendShowClient();
-                switchMessage(Switch.ERROR_NOTI, { title: TemplateText.errorTitle, message: TemplateText.noHotApp(event.keycode -1), hotApp: hotApp });
+                switchMessage(Switch.ERROR_NOTI, { title: TemplateText.errorTitle, message: TemplateText.noHotApp(event.keycode - 1), hotApp: hotApp });
             }
         } else {
             if (event.rawcode >= 48 && event.rawcode <= 58) {
@@ -85,47 +83,93 @@ function fnMethod(event) {
     }
 }
 
-/**
- * Fires on user's keyup
- */
+if (process.platform == 'win32') {
 
- ioHook.on('keyup', event => {
-    // if altgr is disabled do not show dock...
-    if (disableKeyUpListen && event.rawcode != 164) {
-        if(disableKeyUpListenTimeout) clearTimeout(disableKeyUpListenTimeout);
-        disableKeyUpListenTimeout = setTimeout(() => {
-            clearTimeout(disableKeyUpListenTimeout)
-            disableKeyUpListen = false;
-           }, 1000);
-        return;
-    };
-    fnMethod(event);
-});
+    /**
+    * Fires on user's keyup
+    */
 
-
-/**
- * Fires on user's keydown
- */
-ioHook.on('keydown', event => {
-    if (event.altKey) {
-        // focus on swicth to prevent keystokes entering other apps..
-        bingSwitchToFocus(clientPID);
-        // If alt key is pressed, show dock
-        // if altgr is disabled do not show...
-        if(config.disableAltGr && event.rawcode == 165) {
-            disableKeyUpListen = true;
+    ioHook.on('keyup', event => {
+        // if altgr is disabled do not show dock...
+        if (disableKeyUpListen && event.rawcode != 164) {
+            if (disableKeyUpListenTimeout) clearTimeout(disableKeyUpListenTimeout);
+            disableKeyUpListenTimeout = setTimeout(() => {
+                clearTimeout(disableKeyUpListenTimeout)
+                disableKeyUpListen = false;
+            }, 1000);
             return;
+        };
+        fnMethod(event);
+    });
+
+    /**
+     * Fires on user's keydown
+     */
+    ioHook.on('keydown', event => {
+        if (event.altKey) {
+            // If alt key is pressed, show dock
+            // if altgr is disabled do not show...
+            if (config.disableAltGr && event.rawcode == 165) {
+                disableKeyUpListen = true;
+                return;
+            }
+            interChannel.sendShowClient();
         }
-        interChannel.sendShowClient();
+    });
+}
+
+if (process.platform == "darwin") {
+
+    // MacOS switching strategy ...
+
+    const numKeys = [
+        // 1
+        { keycode: 2, rawcode: 18 },
+        // 2
+        { keycode: 3, rawcode: 19 },
+        // 3
+        { keycode: 4, rawcode: 20 },
+        // 4
+        { keycode: 5, rawcode: 21 },
+        // 5
+        { keycode: 6, rawcode: 23 },
+        // 6
+        { keycode: 7, rawcode: 22 },
+        // 7
+        { keycode: 8, rawcode: 26 },
+        // 8
+        { keycode: 9, rawcode: 28 },
+        // 9
+        { keycode: 10, rawcode: 25 },
+        // 0
+        { keycode: 11, rawcode: 29 }
+    ];
+
+    // regsiter a num keys with comand+option as shortcuts
+    for (const current of numKeys) {
+
+        ioHook.registerShortcut([3676, 3640, current.keycode], (keys) => {
+            console.log('Cmd+Option+' + (current.keycode - 1), keys, current)
+            interChannel.sendShowClient();
+            react(current);
+        });
+
     }
-});
+
+    // When user tabs Comand+Options
+    ioHook.registerShortcut([3676, 3640], () => {
+        interChannel.sendShowClient();
+    });
+
+
+}
 
 // Register and start hook.
 (checkDevMode()) ? ioHook.start(true) : ioHook.start();
 
 // Registers the on toast click event handler.
 registerNotifierOnClick();
- 
+
 /**
  * Fires when hot apps list 
  * update is recieved from client
